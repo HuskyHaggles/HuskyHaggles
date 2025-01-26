1368a98aca95d49ed2ed63f2c821b14295ae9cc5
seanfinchg <seanfinchg@gmail.com>
2025-01-26 13:53:13 -0500

    Push Hack Session 2

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <p className="text-blue-200">
+        Made by: Jake Leigh, Kenny Chen, Sean Finch, Vitor Prates
+      </p>
+    </div>
+  );
+}
