af731cf9335459cc2294a309b8f31e622f3dd15f
seanfinchg <seanfinchg@gmail.com>
2025-02-02 12:17:07 -0500

    Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 export default function Navbar(){
     return(
-
+    <div id = "Navbar">
+     <Link to="/Home" id="Home">Home</Link><br/>
+     <Link to="/About" id="Home">Home</Link><br/>
+     <Link to="/Cart" id="Home">Home</Link><br/>
+    </div>
     )
 }
